0555d7f669701ff43c22cc953227b0bc09a2baca
Zther <Zther@Zther-Dsk>
2021-03-24 11:24:29 +0100

    commit inicial

---
+++ src/paquete/Primo.java
@@ -0,0 +1,10 @@
+package paquete;
+
+import java.util.Scanner;
+
+public class Primo {
+	public static void main (String arg[]) {
+		Scanner ent = new Scanner(System.in);
+		System.out.println("Buenos días Ceinmark, soy Ezequiel Sanchez \nIntroduce un número para averiguar si es primo o no:");
+	}
+}
